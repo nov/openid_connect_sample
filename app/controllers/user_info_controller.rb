6b8027079fa55690c74d03d79573fc8f1c8fcb1d
nov matake <nov@matake.jp>
2011-09-12 19:44:25 +0900

    try to handle FB/Google API errors properly at user_info endpoint

--- app/controllers/user_info_controller.rb
+++ app/controllers/user_info_controller.rb
@@ -1,6 +1,20 @@
 class UserInfoController < ApplicationController
   before_filter :require_user_access_token
 
+  rescue_from FbGraph::Exception, Rack::OAuth2::Client::Error do |e|
+    provider = case e
+    when FbGraph::Exception
+      'Facebook'
+    when Rack::OAuth2::Client::Error
+      'Google'
+    end
+    raise Rack::OAuth2::Server::Resource::Bearer::BadRequest.new(:invalid_request, <<-MESSAGE
+Your access token is valid, but we failed to fetch profile data from #{provider}.
+#{provider}'s access token on our side seems expired/revoked.
+MESSAGE
+    )
+  end
+
   def show
     render json: current_token.account.to_response_object(current_token)
   end
