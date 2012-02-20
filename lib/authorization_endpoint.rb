442f71d87183560c3cd231628669c9925a055823
nov matake <nov@matake.jp>
2012-02-20 11:57:44 +0900

    response_type=code+token+id_token support

--- lib/authorization_endpoint.rb
+++ lib/authorization_endpoint.rb
@@ -1,5 +1,3 @@
-require 'rack/oauth2/server/authorize/extension/code_and_token'
-
 class AuthorizationEndpoint
   attr_accessor :app, :account, :client, :redirect_uri, :response_type, :scopes
   delegate :call, to: :app
@@ -12,53 +10,43 @@ class AuthorizationEndpoint
       @scopes = req.scope.inject([]) do |_scopes_, scope|
         _scopes_ << Scope.find_by_name(scope) or req.invalid_scope! "Unknown scope: #{scope}"
       end
-      if allow_approval
-        if approved
-          approved! req, res
+      case req.response_type
+      when :code, :token, :id_token, [:code, :token], [:code, :id_token], [:id_token, :token], [:code, :id_token, :token]
+        if allow_approval
+          if approved
+            approved! req, res
+          else
+            req.access_denied!
+          end
         else
-          req.access_denied!
+          @response_type = req.response_type
         end
       else
-        @response_type = req.response_type
+        req.unsupported_response_type!
       end
     end
   end
 
   def approved!(req, res)
-    case req.response_type
-    when :code, :token, :id_token, [:code, :token], [:code, :id_token], [:id_token, :token]
-      response_types = Array(req.response_type)
-      if response_types.include? :code
-        authorization = account.authorizations.create!(client: @client, redirect_uri: res.redirect_uri, nonce: req.nonce)
-        authorization.scopes << scopes
-        res.code = authorization.code
-      end
-      if response_types.include? :token
-        access_token = account.access_tokens.create!(client: @client)
-        access_token.scopes << scopes
-        res.access_token = access_token.to_bearer_token
-        if access_token.accessible?(Scope::OPENID)
-          # NOTE:
-          #  Not sure id_token should be returned here.
-          #  Will follow spec updates.
-          attach_id_token(req, res)
-        end
-      end
-      if response_types.include? :id_token
-        attach_id_token(req, res)
-      end
-    else
-      res.unsupported_response_type!
+    response_types = Array(req.response_type)
+    if response_types.include? :code
+      authorization = account.authorizations.create!(client: @client, redirect_uri: res.redirect_uri, nonce: req.nonce)
+      authorization.scopes << scopes
+      res.code = authorization.code
+    end
+    if response_types.include? :token
+      access_token = account.access_tokens.create!(client: @client)
+      access_token.scopes << scopes
+      res.access_token = access_token.to_bearer_token
+    end
+    if response_types.include? :id_token
+      res.id_token = account.id_tokens.create!(
+        client: @client,
+        nonce: req.nonce
+      ).to_response_object(
+        scopes.include?(Scope::PPID)
+      ).to_jwt IdToken.config[:private_key]
     end
     res.approve!
-  end
-
-  def attach_id_token(req, res)
-    res.id_token = account.id_tokens.create!(
-      client: @client,
-      nonce: req.nonce
-    ).to_response_object(
-      scopes.include?(Scope::PPID)
-    ).to_jwt IdToken.config[:private_key]
   end
 end
