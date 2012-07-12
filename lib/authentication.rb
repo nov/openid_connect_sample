9ec39b6b6e71855f5c7e0aa27ad25d01373e2179
nov matake <nov@matake.jp>
2012-07-12 23:46:12 +0900

    proper error response for no access token

--- lib/authentication.rb
+++ lib/authentication.rb
@@ -55,7 +55,10 @@ module Authentication
 
   def require_access_token
     @current_token = request.env[Rack::OAuth2::Server::Resource::ACCESS_TOKEN]
-    unless @current_token.accessible? required_scopes
+    unless @current_token
+      raise Rack::OAuth2::Server::Resource::Bearer::Unauthorized.new
+    end
+    unless @current_token.try(:accessible?, required_scopes)
       raise Rack::OAuth2::Server::Resource::Bearer::Forbidden.new(:insufficient_scope)
     end
   end
